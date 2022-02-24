52fea116ba56eac2f592c1bf9d5feadc44debdf5
Michelle <mchristine.stone91@gmail.com>
2022-02-23 22:38:04 -0500

    created the Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,15 +1,31 @@
-const { Shcema, model, Types } = require("mongoose");
-const { schema } = require("./User");
+const { Schema, model, Types } = require("mongoose");
 // Need date format.
 
-const ThoughtSchema = new schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    minLength: 1,
-    maxLenght: 280,
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLenght: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // Need to create this.
+      get: (createAtVal) => dateFormat(createdAtVal),
+    },
   },
-});
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual to get reaction count.
 
 const Thought = model("Thought", ThoughtSchema);
 
