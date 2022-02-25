c4372b65c0972769f91b659f0a36b96056b195cb
Michelle <mchristine.stone91@gmail.com>
2022-02-25 18:04:35 -0500

    added delete thought to thought-controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -57,6 +57,31 @@ const thoughtController = {
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.json(err));
   },
+
+  //   Remove thought.
+  removeThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.thoughtId })
+      .then((deletedThought) => {
+        if (!deletedThought) {
+          return res
+            .status(404)
+            .json({ message: "No thought found with this id!" });
+        }
+        return User.findOneAndUpdate(
+          { _id: params.UserId },
+          { $pull: { thoughts: params.thoughtId } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
 };
 
 module.exports = thoughtController;
