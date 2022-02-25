7377ad1549c90c371ce040d1c081b1f659a57d24
Michelle <mchristine.stone91@gmail.com>
2022-02-25 17:59:18 -0500

    added addReaction and removeReaction routes to thought controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -24,4 +24,39 @@ const thoughtController = {
       })
       .catch((err) => res.json(err));
   },
+
+  // Update an existing though.
+  // Passing params as a parameter, so make sure pass it to addReaction.
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughId },
+      { $push: { reactions: body } },
+      { new: true, reunValidators: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  //   Uses ${pull} operator to remove the specific reply from the replies where reactionId matches the value of params.reactionId.
+  removeReaction({ params }, res) {
+    Thought.findOneAndDelete(
+      { _id: params.thoughtId },
+      {
+        $pull: {
+          reactions: { reactionId: params.reactionId },
+        },
+      },
+      { new: true }
+    )
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
 };
+
+module.exports = thoughtController;
