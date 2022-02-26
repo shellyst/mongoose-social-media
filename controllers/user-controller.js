0ed5c776036543f4c2bf81c34ac80652d46b41bc
Michelle <mchristine.stone91@gmail.com>
2022-02-26 17:28:27 -0500

    added add and delete friend options to user controllers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -75,6 +75,33 @@ const userController = {
       })
       .catch((err) => res.status(400).json(err));
   },
+
+  // Add friend.
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $push: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  deleteFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: { friendId: params.friendId } } },
+      { new: true }
+    )
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(400).json(err));
+  },
 };
 
 module.exports = userController;
