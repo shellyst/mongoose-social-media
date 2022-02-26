a6cc7554eb62e13759606d9158dd3fe5b129bf64
Michelle <mchristine.stone91@gmail.com>
2022-02-25 19:51:33 -0500

    fixed minor errors prevent server from starting

--- models/User.js
+++ models/User.js
@@ -12,7 +12,6 @@ const UserSchema = new Schema(
       type: String,
       unique: true,
       required: true,
-      validate: [validateEmail, "Please fill a valid email address."],
       match: [
         /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
         "Please fill a valid email address",
--- routes/api/index.js
+++ routes/api/index.js
@@ -5,7 +5,7 @@ const router = require("express").Router();
 const userRoutes = require("./user-routes");
 const thoughtRoutes = require("./thought-routes");
 
-router.user("/users", userRoutes);
-router.thought("/thoughts", thoughtRoutes);
+router.use("/users", userRoutes);
+router.use("/thoughts", thoughtRoutes);
 
 module.exports = router;
