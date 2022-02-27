d5ea0dd672b0c1fd203150946fef0bfc75305af7
Michelle <mchristine.stone91@gmail.com>
2022-02-27 18:39:03 -0500

    made correction to delete friend route to fix error found while testing routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -96,10 +96,16 @@ const userController = {
   deleteFriend({ params }, res) {
     User.findOneAndUpdate(
       { _id: params.userId },
-      { $pull: { friends: { friendId: params.friendId } } },
+      { $pull: { friends: params.friendId } },
       { new: true }
     )
-      .then((dbUserData) => res.json(dbUserData))
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
       .catch((err) => res.status(400).json(err));
   },
 };
